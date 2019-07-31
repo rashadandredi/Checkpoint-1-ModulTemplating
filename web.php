d3042098c014d06069fafd88d322fc104af2c93f
Rashad <46430370+rashadandredi@users.noreply.github.com>
2019-07-31 10:15:30 +0700

    selesai

---
+++ web.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    //return view('welcome');
+    echo "Selamat Datang Rashad";
+});
+Route::get('/satu', function () {
+    //return view('welcome');
+    echo "One";
+});
+Route::get('/dua', function () {
+    //return view('welcome');
+    echo "Two";
+});
+Route::get('/tiga', function () {
+    //return view('welcome');
+    echo "Three";
+});
+Route::get('/empat', function () {
+    //return view('welcome');
+    echo "Four";
+});
+Route::get('/lima', function () {
+    //return view('welcome');
+    echo "five";
+});
+Route::get('/enam', function () {
+    //return view('welcome');
+    echo "six";
+});
+Route::get('/tujuh', function () {
+    //return view('welcome');
+    echo "seven";
+});
+Route::get('/delapan', function () {
+    //return view('welcome');
+    echo "eight";
+});
+Route::get('/sembilan', function () {
+    //return view('welcome');
+    echo "nine";
+});
+
+//1. echo langsung nested
+Route::get('/sepuluh', function () {
+    //return view('welcome');
+    echo "ten";
+});
+
+
+//2. manggil view
+Route::get('/sepuluh', function () {
+    return view('rashad');
+  //lokasi view
+});
+
+Route::get('/nemplate', function () {
+    return view('template');
+  //lokasi view
+});
+
+Route::get('/CleaningService', function () {
+    return view('tables');
+  //lokasi view
+});
+
+//Manggil Controller
+Route::get('/usang', 'UsangController@index');
+/* file controller namanya usangController
+    nama url usang
+    index nama functionnya
+*/
+Route::get('/jeruk', 'UsangController@godog');
+Route::get('/index', 'Kontak@index');
+Route::get('/CleaningService', 'CleanerController@tables', function(){
+  return view('tables');
+});
